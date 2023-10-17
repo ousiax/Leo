598b7e0e7b6d3ec55ba873ced80c6e41d34ba98a
Jon.X <ousiax@hotmail.com>
2023-10-17 15:38:50 +0800

    Update Leo.Web/Program.cs to launch web host service standalone.

--- src/Leo.Web/Program.cs
+++ src/Leo.Web/Program.cs
@@ -4,6 +4,16 @@
     {
         static void Main(string[] args)
         {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var startup = new Startup();
+            startup.ConfigureServices(builder.Services);
+
+            var app = builder.Build();
+
+            startup.Configure(app, app.Environment);
+
+            app.Run();
         }
     }
 }
