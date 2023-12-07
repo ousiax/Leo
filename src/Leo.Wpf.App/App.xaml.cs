02536c4a35e4ed271c5f1f3c3141cf1d91e76282
Jon.X <ousiax@hotmail.com>
2023-12-07 19:34:29 +0800

    Using Generic Host to launch the main window on WPF

---
+++ src/Leo.Wpf.App/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Leo.Wpf.App.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace Leo.Wpf.App;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private IHost _host;
+
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((ctx, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await _host.StartAsync();
+
+        var mainWin = _host.Services.GetRequiredService<MainWindow>();
+        mainWin.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        using (_host)
+        {
+            await _host.StopAsync();
+        }
+        base.OnExit(e);
+    }
+}
+
