2812d53abace5be3a9c216ee4da0a95eb63601a4
Jon.X <ousiax@hotmail.com>
2023-12-09 22:47:48 +0800

    Add GenderToStringConverter.cs

---
+++ src/Leo.Wpf.App/Converters/GenderToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using Leo.Data.Domain.Entities;
+using Leo.Wpf.App.Infrastructure;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Leo.Wpf.App.Converters
+{
+    public class GenderToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string key)
+            {
+                return Constants.Genders[key];
+            }
+            else if (value is Gender gender)
+            {
+                return Constants.Genders[gender.ToString()];
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
